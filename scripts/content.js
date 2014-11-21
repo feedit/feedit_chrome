3e40e3f04be44678e8cc625e27408b7ee11ed63e
dafeng.xdf <dafeng.xdf@taobao.com>
2014-11-21 11:14:55 +0800

    Add

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,2 @@
 var get_title = document.getElementsByTagName('title')[0];
 var get_url = location.href;
-
--- scripts/pop.js
+++ scripts/pop.js
@@ -1,10 +1,10 @@
 (function(global){
-    function execClick(){
-        chrome.tabs.executeScript(null, {
-            code: 'var get_title=encodeURI(document.title),get_url=location.href;if(get_title&&get_url){function PostData(){var b="http://www.xdf.me/reader/submit/ajax_submit.php?canshu1="+get_title+"&canshu2="+get_url;var c=document.getElementsByTagName("head"),a=document.createElement("script");a.type="text/javascript";a.src=b;c[0].appendChild(a)}PostData()};'
-        });
-        global.close();
-    }
-    var get_button = document.getElementById('button');
-    get_button.addEventListener('click', execClick);
+  function execClick(){
+    chrome.tabs.executeScript(null, {
+      code: 'var get_title=encodeURI(document.title),get_url=encodeURI(location.href);if(get_title&&get_url){function PostData(){var b="http://www.xdf.me/reader/submit/ajax_submit.php?title="+get_title+"&url="+get_url;var c=document.getElementsByTagName("head"),a=document.createElement("script");a.type="text/javascript";a.src=b;c[0].appendChild(a)}PostData()};'
+    });
+    global.close();
+  }
+  var get_button = document.getElementById('button');
+  get_button.addEventListener('click', execClick);
 })(window)
